7cab5d66342b29b42f2451c97c35affdd4bde189
Abdul Rehman Khan <129224315+AbdulRehmanUNI@users.noreply.github.com>
2023-12-24 13:53:54 +0500

    update:
    hashmap to arraylist

--- Parser.java
+++ Parser.java
@@ -1,9 +1,11 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.io.IOException;
@@ -15,26 +17,26 @@ public class Parser {
 
     public static void tokenize(String input) {
         String[] parts = input.split("\\s+");
-        Map<String, String> tokens = new LinkedHashMap<>();
+        List<String> tokens = new ArrayList<>();
 
         for (String part : parts) {
             if (KEYWORDS.contains(part)) {
-                tokens.put(part, "KEYWORD");
+                tokens.add(part + " KEYWORD");
             } else if (OPERATORS.contains(part)) {
-                tokens.put(part, "OPERATOR");
+                tokens.add(part + " OPERATOR");
             } else if (PUNCTUATION.contains(part)) {
-                tokens.put(part, "PUNCTUATION");
+                tokens.add(part + " PUNCTUATION");
             } else if (part.matches("[a-zA-Z_][a-zA-Z0-9_]*")) {
-                tokens.put(part, "IDENTIFIER");
+                tokens.add(part + " IDENTIFIER");
             } else if (part.matches("[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?")) {
-                tokens.put(part, "NUMBER");
+                tokens.add(part + " NUMBER");
             } else {
-                tokens.put(part, "UNKNOWN");
+                tokens.add(part + " UNKNOWN");
             }
         }
 
-        for (Map.Entry<String, String> entry : tokens.entrySet()) {
-            System.out.println("<" + entry.getValue() + ", " + entry.getKey() + ">");
+        for (String token : tokens) {
+            System.out.println(token);
         }
     }
 
