e35a702a37ebd4c1639cb8a09aba8e70bbcc3507
Abdul Rehman Khan <129224315+AbdulRehmanUNI@users.noreply.github.com>
2023-12-24 19:48:58 +0500

    .

--- Parser.java
+++ Parser.java
@@ -25,17 +25,29 @@ public class Parser {
     }
 
     public static void tokenize(String input) {
-
+        input=input.trim();
         boolean inQuotes = false;
         StringBuilder modifiedInput = new StringBuilder();
-        for (char c : input.toCharArray()) {
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
             if (c == '\"') inQuotes = !inQuotes;
-            if (inQuotes && c == ' ') modifiedInput.append("FUCKYOU!");
-            else modifiedInput.append(c);
+            if (inQuotes && c == ' ') modifiedInput.append("DUCKYOU!");
+            else if (c == '=' && !inQuotes) {
+                if (i < input.length() - 1 && input.charAt(i + 1) == '=') {
+                    // If the current and next characters are both '=', append them without spaces
+                    modifiedInput.append("==");
+                    i++; // Skip the next character
+                } else {
+                    // If the current character is '=' and it's not part of '==', add spaces around it
+                    modifiedInput.append(" = ");
+                }
+            } else {
+                modifiedInput.append(c);
+            }
         }
     
         input = modifiedInput.toString()
-                .replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)", "")
+                // .replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)", " COMMENT ")
                 .replaceAll("\"(.*?)\"", " \"$1\" ")
                 .replace("+", " + ")
                 .replace("-", " - ")
@@ -45,32 +57,49 @@ public class Parser {
                 .replace(")", " ) ")
                 .replace("{", " { ")
                 .replace("}", " } ")
-                .replace(";", " ; ");
+                .replace(";", " ; ")
+                .replace("==", " == ")
+                .replace(">=", " >= ")
+                .replace("<=", " <= ")
+                .replace(">", " > ")
+                .replace("<", " < ")
+                .replace("=>", " => ");
     
         String[] parts = input.split("\\s+");
-        List<String> tokens = new ArrayList<>();
     
         for (String part : parts) {
-            if(part==" ") continue;
             if (part.startsWith("\"") && part.endsWith("\"")) {
-                part=part.replace("FUCKYOU!"," ");
+                part=part.replace("DUCKYOU!"," ");
                 System.out.println("<STRING, " + part + ">");
             } else if (TOKEN_CLASSES.containsKey(part)) {
-                System.out.println("<" + TOKEN_CLASSES.get(part) + ", " + part + ">");
+                String op=printOperators(part);
+                System.out.println("<" + TOKEN_CLASSES.get(part) + ", " + op+ ">");
             } else if (part.startsWith("//")) {
                 System.out.println("<COMMENT, " + part + ">");
-            }else if (part.matches("[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?")) {
-                tokens.add("<NUMBER, " + part + ">");
-            } else {
-                System.out.println("<UNKNOWN, " + part + ">");
+            }else if(part.matches("[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?")){
+                System.out.println("<NUMBER, "+ part+">");
+            }else if(part.equals("COMMENT")){
+                System.out.println("<COMMENT>");
+            } 
+            else {
+                System.out.println("<ID, " + part + ">");
             }
         }
     }
 
+    public static String printOperators(String str){
+        str=str.trim();  
+        if(str.equals("=")) return "EQ";
+        if(str.equals("=>")) return "GEQ";
+        if(str.equals("<=")) return "LEQ";
+        if(str.equals(">")) return "GT";
+        if(str.equals("<")) return "LT";
+        else return str;
+    }
 
 
     public static void main(String[] args) {
-        String input = " if ( x >= 0 ) { print ( \"Hello World\" ) ; } else { int sum = 0 ; for ( int i = 0 ; i < 10 ; i = i + 1 ) { sum = i + 12.34 + 21E-2 + .21 ; } } ;";
+        String input = "if(x >= 0 ) { print ( \"Hello World\" + \"1.001\" ) ; } else { int sum=0 ; for ( int i = 0 ; i < 10 ; i = i + 1 ) { sum = i + 12.34+21E-2 + .21 ; } a==b } ;";
         tokenize(input);  
     }
 }
--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         String input= "System.out.println(\"Hello World\");";
-        
-    }
+            
+    }   
     
 }
